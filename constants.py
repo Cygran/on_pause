5be53fe146e830743167b2b2d18e6cb91178eae6
Cygran <apcanning90@gmail.com>
2025-02-20 10:37:38 +0000

    Refactor settings loading by removing the old load_settings function and integrating SettingsWindow for improved settings management

--- constants.py
+++ constants.py
@@ -1,20 +1,10 @@
 from platformdirs import user_data_dir
+from settings import SettingsWindow
 import os
-import json
+
+
 
 SETTINGS_PATH = user_data_dir("On Pause", "Cygran")
 SETTINGS_FILE = os.path.join(SETTINGS_PATH, "user_settings.json")
+CURRENT_SETTINGS, SETTINGS_VALID = SettingsWindow.load_settings()
 
-def load_settings():
-    try:
-        if os.path.exists(SETTINGS_FILE):
-            with open(SETTINGS_FILE, 'r', encoding='utf-8') as f:
-                settings = json.load(f)
-                # Check if Agent exists and is a valid number
-                if settings.get("Agent") and settings["Agent"].isdigit():
-                    return settings, True
-        return {"Agent": ""}, False
-    except Exception:
-        return {"Agent": ""}, False
-
-CURRENT_SETTINGS, SETTINGS_VALID = load_settings()
